6f093284d150e3fa0a034adf81a1f1fbde6919f0
pereskokovae <pereskokovae48@gmail.com>
2025-04-18 21:19:01 +0700

    Исправила названия в функциях и переменных

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ import collections
 
 def main():
     excel_read = pandas.read_excel("wine_сatalog.xlsx", sheet_name='Лист1', keep_default_na=False)
-    excel_read_wine = excel_read.to_dict(orient='records')
+    wine_catalog = excel_read.to_dict(orient='records')
     excel_wine = collections.defaultdict(list)
 
 
-    for wine in excel_read_wine:
+    for wine in wine_catalog:
         category = wine['Категория']
         name = wine['Название']
         sort = wine['Сорт']
@@ -23,15 +23,15 @@ def main():
 
 
     today = datetime.datetime.now()
-    year_found_wine = 1920
-    years_together = today.year - year_found_wine
+    wine_found_year = 1920
+    together_years = today.year - wine_found_year
 
 
-    def spell(years_together):
-        if years_together % 100 in range(11, 20):
+    def spell(together_years):
+        if together_years % 100 in range(11, 20):
             return "лет"
         else:
-            last_digit = years_together % 10
+            last_digit = together_years % 10
             if last_digit == 1:
                 return "год"
             elif last_digit in [2, 3, 4]:
@@ -49,8 +49,8 @@ def main():
     template = env.get_template('template.html')
 
     rendered_page = template.render(
-        years_together=years_together,
-        time_period=spell(years_together),
+        together_years=together_years,
+        time_period=spell(together_years),
         excel_wine=excel_wine
         )
 
